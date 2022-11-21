f7e5379118990509d0bf4fb3a3874c830d3aaf68
NaturaAurum <natura0321@gmail.com>
2022-11-22 06:13:24 +0900

    손가락 작업중

--- Assets/VLive/Runtime/HandsSolver.cs
+++ Assets/VLive/Runtime/HandsSolver.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 namespace VLive.Runtime
 {
@@ -31,14 +32,40 @@ namespace VLive.Runtime
         private Transform _leftHand;
         private Transform _rightHand;
 
+        private readonly List<Vector3KalmanFilter> _leftKalmanFilters = new();
+        private readonly List<Vector3KalmanFilter> _rightKalmanFilters = new();
+        private readonly List<OneEuroFilter<Vector3>> _leftOneEuroFilters = new();
+        private readonly List<OneEuroFilter<Vector3>> _rightOneEuroFilters = new();
+        
+        private const double KalmanTimeInterval = 0.45;
+        private const double KalmanNoise = 0.4;
+        private const float MinCutOffValue = 1.5f;
+        private const float DCutOffValue = 1f;
+        private const float Beta = 0.1f;
+        private const float Frequency = 90f;
+
+        private float _leftAngVelocity;
+        private float _rightAngVelocity;
+
+        private const string Left = "Left";
+        private const string Right = "Right";
+
         private void Awake()
         {
             _animator = GetComponent<Animator>();
-            InitializeFingerLocalCoord(ref _leftFingerLocalCoords, "Left");
-            InitializeFingerLocalCoord(ref _rightFingerLocalCoords, "Right");
+            InitializeFingerLocalCoord(ref _leftFingerLocalCoords, Left);
+            InitializeFingerLocalCoord(ref _rightFingerLocalCoords, Right);
 
             _leftHand = _animator.GetBoneTransform(HumanBodyBones.LeftHand);
             _rightHand = _animator.GetBoneTransform(HumanBodyBones.RightHand);
+
+            for (var i = 0; i < 21; ++i)
+            {
+                _leftOneEuroFilters.Add(new OneEuroFilter<Vector3>(Frequency, MinCutOffValue, Beta, DCutOffValue));
+                _rightOneEuroFilters.Add(new OneEuroFilter<Vector3>(Frequency, MinCutOffValue, Beta, DCutOffValue));
+                _leftKalmanFilters.Add( new Vector3KalmanFilter(KalmanTimeInterval, KalmanNoise));
+                _rightKalmanFilters.Add( new Vector3KalmanFilter(KalmanTimeInterval, KalmanNoise));
+            }
         }
 
         private void InitializeFingerLocalCoord(ref Vector3[] fingerLocalCoords, string side)
@@ -63,7 +90,14 @@ namespace VLive.Runtime
         {
             if (data.LeftPosList is {Count: > 20})
             {
+                data.LeftPosList = data.LeftPosList.Select((point, index) =>
+                {
+                    point.Point = _leftKalmanFilters[index].CorrectAndPredict(point.Point);
+                    point.Point = _leftOneEuroFilters[index].Filter(point.Point);
+                    return point;
+                }).ToList();
                 SetHandRotation(_leftHand, data.LeftPosList, false);
+                FingerSolve(_leftHand, data.LeftPosList, false);
             }
             else
             {
@@ -72,11 +106,46 @@ namespace VLive.Runtime
 
             if (data.RightPosList is {Count: > 20})
             {
+                data.RightPosList = data.RightPosList.Select((point, index) =>
+                {
+                    point.Point = _rightKalmanFilters[index].CorrectAndPredict(point.Point);
+                    point.Point = _rightOneEuroFilters[index].Filter(point.Point);
+                    return point;
+                }).ToList();
                 SetHandRotation(_rightHand, data.RightPosList, true);
+                FingerSolve(_rightHand, data.RightPosList, true);
             }
             else
             {
                 _rightHand.localRotation = Quaternion.identity;
+            }
+        }
+
+        private void FingerSolve(Transform hand, IReadOnlyList<PointData> points, bool rightHand)
+        {
+            var quaternion12 = hand.localRotation * hand.transform.parent.localRotation;
+            var localCoords = rightHand ? _rightFingerLocalCoords : _leftFingerLocalCoords;
+            var str = rightHand ? Right : Left;
+            for (var i = 0; i < FingerNames.Length; ++i)
+            {
+                var quaternion13 = quaternion12;
+                for(var j = 0; j < PartNames.Length - 1; ++j)
+                {
+                    var index3 = i * 4 + 1 + j;
+                    var normalized14 = localCoords[index3 + 1].normalized; // 14
+                    var quaternion14 = quaternion13;
+                    var vector3_1 = points[index3 + 1].Point - points[index3].Point; // 3_1
+                    var normalized15 = vector3_1.normalized;
+                    var vector3_3 = quaternion14 * normalized15; // 3_3
+                    var vector3_2 = Vector3.Cross(localCoords[index3 + 1], Vector3.up);
+                    var normalized16 = vector3_2.normalized; // 16
+                    var vector3_4 = Vector3.ProjectOnPlane(vector3_3, normalized16);
+                    var normalized17 = vector3_4.normalized; // 17
+                    var rotation4 = Quaternion.FromToRotation(normalized14, normalized17); // rotation4
+                    var quaternion15 = Quaternion.FromToRotation(rotation4 * normalized14, vector3_3) * rotation4; // quaternion15
+                    _animator.GetBoneTransform(Enum.Parse<HumanBodyBones>(str + FingerNames[i] + PartNames[j])).localRotation = quaternion15;
+                    quaternion13 = Quaternion.Inverse(quaternion15) * quaternion13;
+                }
             }
         }
 
@@ -91,8 +160,20 @@ namespace VLive.Runtime
             var rightDir = palm3 - palm0;
             var upDir = rightHand ? _handPlane.normal : _handPlane.flipped.normal;
             var forwardDir = Vector3.Cross(rightDir, upDir);
-            var handMatrix = new Matrix4x4(rightHand ? -rightDir : rightDir, upDir, rightHand ? -forwardDir : forwardDir, RightColumn);
-            hand.localRotation = handMatrix.rotation;
+            var handMatrix = new Matrix4x4(rightHand ? rightDir : -rightDir, upDir, rightHand ? forwardDir : -forwardDir, RightColumn);
+            
+            // lower arm
+            var dir = hand.localPosition;
+            var lowerArm = hand.transform.parent;
+            var angle = Vector3.Angle(lowerArm.up, handMatrix.rotation * Vector3.up);
+            var lowerArmRot = Quaternion.AngleAxis(rightHand ? -angle : angle, dir);
+            var rotation = lowerArm.rotation;
+            lowerArmRot = rotation * lowerArmRot;
+            // rotation = Quaternion.Slerp(rotation, lowerArmRot, Smooth * Time.deltaTime);
+            lowerArm.rotation = lowerArmRot;
+            hand.rotation = handMatrix.rotation;
         }
+
+        private const float Smooth = 10f;
     }
 }
---
+++ Assets/VLive/Runtime/Vector3KalmanFilter.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+namespace VLive.Runtime
+{
+    public class Vector3KalmanFilter
+    {
+        private readonly DiscreteKalmanFilter<ConstantVelocity3DModel, Vector3> _kalmanFilter;
+        private int _effectiveCount;
+        private const int StartCount = 50;
+        
+        public Vector3KalmanFilter(double timeInterval, double noise)
+        {
+            _kalmanFilter = new DiscreteKalmanFilter<ConstantVelocity3DModel, Vector3>(
+                new ConstantVelocity3DModel()
+                {
+                    Position = Vector3.zero,
+                    Velocity = Vector3.zero
+                },
+                ConstantVelocity3DModel.GetProcessNoise(noise, timeInterval),
+                3,
+                0,
+                ConstantVelocity3DModel.ToArray,
+                ConstantVelocity3DModel.FromArray,
+                joint => new double[]
+                {
+                    joint.x,
+                    joint.y,
+                    joint.z
+                }
+            );
+
+            _kalmanFilter.ProcessNoise = ConstantVelocity3DModel.GetProcessNoise(noise, timeInterval);
+            _kalmanFilter.MeasurementNoise = MatrixFunctions.Diagonal(_kalmanFilter.MeasurementVectorDimension, 1.0);
+            _kalmanFilter.MeasurementMatrix = ConstantVelocity3DModel.GetPositionMeasurementMatrix();
+            _kalmanFilter.TransitionMatrix = ConstantVelocity3DModel.GetTransitionMatrix(timeInterval);
+            _kalmanFilter.Predict();
+        }
+
+        public void UpdateFilterParameter(double timeInterval, double noise)
+        {
+            _kalmanFilter.ProcessNoise = ConstantVelocity3DModel.GetProcessNoise(noise, timeInterval);
+            _kalmanFilter.TransitionMatrix = ConstantVelocity3DModel.GetTransitionMatrix(timeInterval);
+            _effectiveCount = 0;
+        }
+
+        public void Correct(Vector3 v) => _kalmanFilter.Correct(v);
+        public void Predict() => _kalmanFilter.Predict();
+        public Vector3 GetPosition() => _kalmanFilter.State.Position;
+
+        public Vector3 CorrectAndPredict(Vector3 v)
+        {
+            Correct(v);
+            Predict();
+            if (_effectiveCount >= 50)
+            {
+                return GetPosition();
+            }
+
+            ++_effectiveCount;
+            return v;
+        }
+    }
+}
